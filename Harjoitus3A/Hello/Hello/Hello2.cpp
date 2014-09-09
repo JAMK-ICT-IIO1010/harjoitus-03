42c4b2d399024c96a52e1858ee1a258358bed830
Sami Liimatainen <H8286@student.jamk.fi>
2014-09-09 18:05:32 +0300

    Harjoitus3

---
+++ Harjoitus3A/Hello/Hello/Hello2.cpp
@@ -0,0 +1,23 @@
+/*Harjoitus 5
+Sami Liimatainen
+IIO14S1
+9.9.2014
+Versio 1.0*
+
+Tulostetaan hello world joka muutettiin tunnilla tulostamaan Hei Hello 2
+*/
+
+
+
+/*kommentti alkaa t‰st‰ 
+ja jatkuu
+ja jatkuu...*/
+#include <iostream> //t‰ss‰ m‰‰ritell‰‰n otsikkotiedosto
+using namespace std;
+void main(void)
+{
+	cout << "Hei Hello 2!\n"
+		<< "Hyvin menee "
+		<< "mutta menkˆˆn\n" << endl;
+	cout << "lis‰tulostus" << endl;
+}
